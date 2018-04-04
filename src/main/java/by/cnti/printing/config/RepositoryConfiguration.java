93172918fde3cf4160d29a95ee671314192184c6
AndrewEvl <Lino220037@gmail.com>
2018-04-04 20:40:14 +0300

    lille fix

--- src/main/java/by/cnti/printing/config/RepositoryConfiguration.java
+++ src/main/java/by/cnti/printing/config/RepositoryConfiguration.java
@@ -1,4 +1,4 @@
-package by.cnti.printing.conig;
+package by.cnti.printing.config;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
